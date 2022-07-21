c1b3f6f4e6f0b90990989ca1d4c3970ab536f88c
Yerraguntla Satyakama Dattatreya <satya.kama@cloudreach.com>
2022-07-21 14:49:14 +0530

    adding some changes

--- route.tf
+++ route.tf
@@ -16,7 +16,7 @@ resource "aws_route_table" "nat_route_tbl" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.nat_gateway.id
+    gateway_id = aws_nat_gateway.nat_gateway.id
   }
 
   tags = {
